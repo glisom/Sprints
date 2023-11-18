a83b1f28e8bf707fe47e0de4c1652bb63a840394
glisom <glisom@icloud.com>
2023-11-17 22:03:53 -0600

    Initial Commit

---
+++ Sprints/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Sprints
+//
+//  Created by Grant Isom on 11/17/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
